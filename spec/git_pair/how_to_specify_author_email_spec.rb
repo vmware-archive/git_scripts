7f30126581914fb3f9fed5ccd8c8e2d0e856d944
Ben Biddington <ben.biddington@gmail.com>
2015-11-20 21:59:03 +1300

    [WIP, FAILING, START-HERE] Tag 'me' and make the other person the author

--- spec/git_pair/how_to_specify_author_email_spec.rb
+++ spec/git_pair/how_to_specify_author_email_spec.rb
@@ -8,7 +8,27 @@ describe 'How to specify what value to use for user.email' do
     @git = MockGitConfig.new
   end
 
-  it 'sets author.email to the email address for whomever you specify as the author in the email settings' do
+  it '[START HERE] a better way to go may be to specify "me" because the author needs to vary with the pair -- it cannot be a static setting' do
+    config = {
+      'me' => 'bb',
+      'pairs' => {
+        'bb' => "Ben Biddington; ben.biddington",
+        'rf' => "Richard Bizzness; ricky.bizzness",
+        'tb' => "Tim Biddington; mud.man",
+        },
+      'email' => {'domain' => 'aol.com'}
+    }
+    
+    UseCases::GitPair.apply(:initials => ['bb','rf'], :git => @git.fun, :config => config)
+    
+    @git.must_have_received(hash_including(:email => "ricky.bizzness@aol.com"))
+
+    UseCases::GitPair.apply(:initials => ['bb','tb'], :git => @git.fun, :config => config)
+    
+    @git.must_have_received(hash_including(:email => "mud.man@aol.com"))
+  end
+  
+  it '[NO-LONGER-VALID] sets author.email to the email address for whomever you specify as the author in the email settings' do
     config = {
       'pairs' => {
         'bb' => "Ben Biddington; ben.biddington",
@@ -24,8 +44,6 @@ describe 'How to specify what value to use for user.email' do
     @git.must_have_received(hash_including(:email => "ricky.bizzness@aol.com"))
   end
 
-  it 'does what when email author alias is not present in pairs?'
-  
   it 'sets author.email to a combination of email addresses by default' do
     config = {
       'pairs' => {
