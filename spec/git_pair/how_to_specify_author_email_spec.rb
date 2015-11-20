550753163e3f70f16247476a04199f22503f7671
Ben Biddington <ben.biddington@gmail.com>
2015-11-20 20:48:43 +1300

    How the email config affects `author.email`

---
+++ spec/git_pair/how_to_specify_author_email_spec.rb
@@ -0,0 +1,35 @@
+require 'pivotal_git_scripts/use_cases/git_pair'
+require 'support'
+
+describe 'How to specify what value to use for user.email' do
+  before do
+    @git = MockGitConfig.new
+  end
+  
+  it 'sets author.email to a combination of email addresses by default' do
+    config = {
+      'pairs' => {
+        'bb' => "Ben Biddington; ben.biddington",
+        'rf' => "Richard Bizzness; ricky.bizzness",
+        },
+      'email' => {'domain' => 'aol.com'}
+    }
+    
+    UseCases::GitPair.apply(:initials => ['bb','rf'], :git => @git.fun, :config => config)
+    
+    @git.must_have_received(hash_including(:email => "ben.biddington+ricky.bizzness@aol.com"))
+  end
+
+  it 'does not set author.email at all when no email setting is present in config' do
+    config_with_missing_email_setting = {
+      'pairs' => {
+        'bb' => "Ben Biddington; ben.biddington",
+        'rf' => "Richard Bizzness; ricky.bizzness",
+        }
+    }
+    
+    UseCases::GitPair.apply(:initials => ['bb','rf'], :git => @git.fun, :config => config_with_missing_email_setting)
+    
+    @git.must_not_have_received(:email)
+  end
+end
--- spec/support.rb
+++ spec/support.rb
@@ -10,4 +10,8 @@ class MockGitConfig
   def must_have_received(expected={})
     expect(@args).to include(expected)
   end
+
+  def must_not_have_received(expected={})
+    expect(@args).to_not include(expected)
+  end
 end
