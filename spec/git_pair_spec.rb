5de9aeda0f0c8fbbd99423e05cd6e76ccadfadc2
Ben Biddingtona and Richard Fortune <ben.biddington@gmail.com>
2015-11-20 13:43:36 +1300

    The order you supply initials does not affect <author.name> or <author.email>

--- spec/git_pair_spec.rb
+++ spec/git_pair_spec.rb
@@ -6,18 +6,51 @@ module UseCases
       def apply(opts={})
         git    = opts[:git] || fail("You need to supply the :git port")
         config = opts[:config] || fail("You need to supply :config. (The current git pair settings.)")
+        initials = opts[:initials] || []
+        
+        author_names, email_ids = extract_author_names_and_email_ids_from_config(config, initials)
+        authors = pair_names(author_names)
+        git_config = {:name => authors,  :initials => initials.sort.join(" ")}
 
-        #author_names, email_ids = extract_author_names_and_email_ids_from_config(config, initials)
-        authors = pair_names(config.map{|c| c[:name]})
-        #git_config              = {:name => authors,  :initials => initials.sort.join(" ")}
-
-        git.config(:author_name => authors)
+        git_config[:email] = build_email(email_ids, config["email"]) unless no_email(config)
+        
+        git.config git_config
       end
 
       private
 
+      def extract_author_names_and_email_ids_from_config(config, initials) # [!] Duplicated from lib/pivotal_git_scripts/git_pair.rb
+        authors = read_author_info_from_config(config, initials)
+        authors.sort!.uniq! # FIXME
+        authors.map do |a|
+          full_name, email_id = a.split(";").map(&:strip)
+          email_id ||= full_name.split(' ').first.downcase
+          [full_name, email_id]
+        end.transpose
+      end
+
+      def read_author_info_from_config(config, initials_ary) # [!] Duplicated from lib/pivotal_git_scripts/git_pair.rb
+        initials_ary.map do |initials|
+          config['pairs'][initials.downcase] or
+            raise GitPairException, "Couldn't find author name for initials: #{initials}. Add this person to the .pairs file in your project or home directory."
+        end
+      end
+
+      def no_email(config)
+        !config.key? 'email'
+      end
+      
       def pair_names(author_names) # [!] Duplicated from lib/pivotal_git_scripts/git_pair.rb
         [author_names[0..-2].join(", "), author_names.last].reject(&:empty?).join(" and ")
+      end
+
+      def build_email(emails, config) # [!] Duplicated from lib/pivotal_git_scripts/git_pair.rb
+        if config.is_a?(Hash)
+          prefix = config['prefix'] if !config['no_solo_prefix'] or emails.size > 1
+          "#{([prefix] + emails).compact.join('+')}@#{config['domain']}"
+        else
+          config
+        end
       end
     end
   end
@@ -27,17 +60,36 @@ describe "\`git pair rb bb\`" do
   before do
     @git = spy("represents the current git repo")
 
-    config = [
-      {:initials => 'bb', :name => "Ben Biddington", :email => "ben.biddington@aol.com"},
-      {:initials => 'rf', :name => "Richard Bizzness", :email => "ricky.bizzness@eire.com"}
-    ]
+    @config = {
+      'pairs' => {
+        'bb' => "Ben Biddington; ben.biddington",
+        'rf' => "Richard Bizzness; ricky.bizzness",
+      
+      },
+      'email' => {
+        'domain' => 'aol.com',
+        'no_solo_prefix' => true}
+    }
     
-    UseCases::GitPair.apply(:initials => ['bb' 'rf'], :git => @git, :config => config)
+    UseCases::GitPair.apply(:initials => ['bb','rf'], :git => @git, :config => @config)
   end
   
   it 'sets author.name to combination of authors' do
-    expect(@git).to have_received(:config).with({
-      :author_name => "Ben Biddington and Richard Bizzness"})
+    expect(@git).to have_received(:config).with(hash_including(:name => "Ben Biddington and Richard Bizzness"))
+  end
+
+  it 'sets author.email to a combination of email addresses' do
+    expect(@git).to have_received(:config).with(hash_including(:email => "ben.biddington+ricky.bizzness@aol.com"))
+  end
+
+  it 'does not matter what order initials are supplied' do
+    git = spy("represents the current git repo")
+    
+    UseCases::GitPair.apply(:initials => ['rf', 'bb'], :git => git, :config => @config)
+
+    expect(git).to have_received(:config).with(hash_including(
+      :name => "Ben Biddington and Richard Bizzness",
+      :email => "ben.biddington+ricky.bizzness@aol.com"))
   end
   
   describe 'that you may configure it such that \`git pair\` sets auther email, too'
